6ae92d0b22b99cfe93df54c3a4ab59e84df7055f
highscot <highscot@gmail.com>
2014-03-27 11:19:00 -0400

    Solver class test code for cusp solver

--- cuda/poisson/tests/test_solver_cusp.cu
+++ cuda/poisson/tests/test_solver_cusp.cu
@@ -1,3 +1,7 @@
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
 #include <math.h>
 #include <cusp/coo_matrix.h>
 #include <cusp/csr_matrix.h>
@@ -7,9 +11,29 @@
 #include "cusp_poisson.hpp"
 #include "solvers.hpp"
 
-int main(void)
+BOOST_AUTO_TEST_SUITE( solver_cusp_3d_tests )
+
+// Check for explosions
+BOOST_AUTO_TEST_CASE( solver_cusp_constructor )
 {
 
+	int nx = 50;
+	int ny = 100;
+	int nz = 150;
+
+	float dx, dy, dz;
+	dx = dy = dz = 1;
+
+	cusp::array1d<float, cusp::device_memory> b(nz*ny*nx, 0);
+
+	PoissonSolver3DCUSP<float> solver(b, nz, dz, ny, dy, nx, dx);
+
+
+}
+
+
+BOOST_AUTO_TEST_CASE( solver_cusp_solver_completes )
+{
 	int nx, ny, nz;
 	nx = ny = nz = 100;
 
@@ -33,5 +57,7 @@ int main(void)
 
 	solver.solve(x);
 
-	return 0;
 }
+
+
+BOOST_AUTO_TEST_SUITE_END()
