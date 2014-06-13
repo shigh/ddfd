f8e79d8d98aa642d7f668088c4d605890bb139c4
shigh <highscot@gmail.com>
2014-06-13 07:21:41 -0400

    Removed files from old repo

--- pde/solver/linalg/linear_solve.hpp
+++
@@ -1,9 +0,0 @@
-
-#ifndef __LINALG_LINEAR_SOLVE_H
-#define __LINALG_LINEAR_SOLVE_H
-
-int d_ge_linear_solve(double* A, double* x, double* b, int N);
-
-int d_ge_linear_solve(double* A, double* x, double* b, int N, int Nb);
-
-#endif
