2bc0d1f6467079eb7de6b40a2a6750496b4ccc37
shigh <highscot@gmail.com>
2014-06-13 08:00:56 -0400

    Started setting up grid dimension structs

--- ddfd/grid.cu
+++ ddfd/grid.cu
@@ -21,3 +21,74 @@ void partition_domain(std::vector<std::size_t>& start, std::vector<std::size_t>&
 	end[n-1] = N;
 
 }
+
+
+LocalGrid::LocalGrid(MPI_Comm cart_comm,
+					 std::vector<int> grid_dim,
+					 GlobalGrid globalGrid) :
+	nz(globalGrid.nz), ny(globalGrid.ny), nx(globalGrid.nx),
+	dz(globalGrid.dz), dy(globalGrid.dy), dx(globalGrid.dx),
+	overlap(globalGrid.overlap)
+{
+
+	// Get local grid location
+	int grid_rank;
+	std::vector<int> grid_coords(3);
+	MPI_Comm_rank(cart_comm, &grid_rank);
+	MPI_Cart_coords(cart_comm, grid_rank, 3, &grid_coords[0]);
+
+	// Determine which neighboors exist
+	has_east   = grid_coords[2] < grid_dim[2]-1;
+	has_west   = grid_coords[2] > 0;
+	has_north  = grid_coords[1] < grid_dim[1]-1;
+	has_south  = grid_coords[1] > 0;
+	has_top    = grid_coords[0] < grid_dim[0]-1;
+	has_bottom = grid_coords[0] > 0;
+
+	// Get neighboor ranks
+	std::vector<int> tmp_coords(3);		
+	east = -1;
+	if(has_east)
+	{
+		tmp_coords = grid_coords;
+		tmp_coords[2] += 1;
+		MPI_Cart_rank(cart_comm, &tmp_coords[0], &east);
+	}
+	west = -1;
+	if(has_west)
+	{
+		tmp_coords = grid_coords;
+		tmp_coords[2] -= 1;
+		MPI_Cart_rank(cart_comm, &tmp_coords[0], &west);
+	}
+	north = -1;
+	if(has_north)
+	{
+		tmp_coords = grid_coords;
+		tmp_coords[1] += 1;
+		MPI_Cart_rank(cart_comm, &tmp_coords[0], &north);
+	}
+	south = -1;
+	if(has_south)
+	{
+		tmp_coords = grid_coords;
+		tmp_coords[1] -= 1;
+		MPI_Cart_rank(cart_comm, &tmp_coords[0], &south);
+	}
+	top = -1;
+	if(has_top)
+	{
+		tmp_coords = grid_coords;
+		tmp_coords[0] += 1;
+		MPI_Cart_rank(cart_comm, &tmp_coords[0], &top);
+	}
+	bottom = -1;
+	if(has_bottom)
+	{
+		tmp_coords = grid_coords;
+		tmp_coords[0] -= 1;
+		MPI_Cart_rank(cart_comm, &tmp_coords[0], &bottom);
+	}
+
+
+}
--- ddfd/grid.hpp
+++ ddfd/grid.hpp
@@ -8,6 +8,54 @@
 #include <math.h>
 
 
+struct GlobalGrid
+{
+
+	// Global grid dimensions
+	const std::size_t nz;
+	const std::size_t ny;
+	const std::size_t nx;
+	const std::size_t overlap;
+
+	// Grid deltas
+	const std::size_t dz;
+	const std::size_t dy;
+	const std::size_t dx;
+
+};
+
+
+struct LocalGrid
+{
+
+	// Local grid dimensions
+	const std::size_t nz;
+	const std::size_t ny;
+	const std::size_t nx;
+	const std::size_t overlap;
+
+	// Grid deltas
+	const std::size_t dz;
+	const std::size_t dy;
+	const std::size_t dx;
+
+	// Global grid locations
+	// Use int because thats what MPI expects
+	int z, y, x;
+	
+	bool has_west, has_east, has_north, has_south,
+		 has_top, has_bottom;
+	int  west, east, north, south,
+		 top, bottom;
+
+	LocalGrid(MPI_Comm cart_comm,
+			  std::vector<int> grid_dim,
+			  GlobalGrid globalGrid);
+
+	
+};
+
+
 /*!
  * Start and endpoints of partitioned domains.
  * The end point generated is the actual array end, not one past the end.
